6f0469980a9d994ae8da0b55af9dfbc43ce1e1ed
joshuabamboo <joshuaowens011@gmail.com>
2015-01-29 11:53:23 -0500

    before erasing comp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,6 @@ class UsersController < ApplicationController
     if session[:fb_auth] != nil
       @facebook_statuses = @user.get_facebook_status_dirt(session[:fb_auth])
       @facebook_photos = @user.get_facebook_photo_dirt
-      binding.pry
       @fb_percentage = (@user.get_facebook_photo_dirt.size / @user.total_fb_photo_count.to_f).round(3)
     end
     render :results
