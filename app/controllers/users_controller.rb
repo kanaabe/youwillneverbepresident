10b32fdfa5cc926b8b2e18c6ea7a63666cd00b9d
joshuabamboo <joshuaowens011@gmail.com>
2016-02-20 16:35:48 -0500

    remove fb/myspace logic from Users#index controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,18 +17,7 @@ class UsersController < ApplicationController
       @all_tweets.size > 0 ? @twitter_percentage = (@tweets.size.to_f/@all_tweets.size.to_f).round(3) : @twitter_percentage = 0
       @twitter_photo = @user.get_twitter_photo
     end
-    
-    if session[:old_email] != ""
-      # @user.old_email = session[:old_email]
-      # @myspace_photo = @user.get_myspace_photo
-    end
 
-    if session[:fb_auth] != nil
-      @facebook_statuses = @user.get_facebook_status_dirt(session[:fb_auth])
-      @facebook_photos = @user.get_facebook_photo_dirt
-      @facebook_name = @user.get_name
-      @fb_percentage = (@user.get_facebook_photo_dirt.size / @user.total_fb_photo_count.to_f).round(3)
-    end
     render :results
   end
 
