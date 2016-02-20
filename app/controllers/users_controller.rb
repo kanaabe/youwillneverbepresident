c95d744bc6b8ae9197cf824d7c9cd9a2ae00f9dc
joshuabamboo <joshuaowens011@gmail.com>
2016-02-20 16:34:18 -0500

    comment out fb/myspace controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,17 +18,17 @@ class UsersController < ApplicationController
       @twitter_photo = @user.get_twitter_photo; p "Users#index ln 18"
     end
     
-    if session[:old_email] != ""
-      # @user.old_email = session[:old_email]
-      # @myspace_photo = @user.get_myspace_photo
-    end
+    # if session[:old_email] != ""
+    #   # @user.old_email = session[:old_email]
+    #   # @myspace_photo = @user.get_myspace_photo
+    # end
 
-    if session[:fb_auth] != nil
-      @facebook_statuses = @user.get_facebook_status_dirt(session[:fb_auth])
-      @facebook_photos = @user.get_facebook_photo_dirt
-      @facebook_name = @user.get_name
-      @fb_percentage = (@user.get_facebook_photo_dirt.size / @user.total_fb_photo_count.to_f).round(3)
-    end
+    # if session[:fb_auth] != nil
+    #   @facebook_statuses = @user.get_facebook_status_dirt(session[:fb_auth])
+    #   @facebook_photos = @user.get_facebook_photo_dirt
+    #   @facebook_name = @user.get_name
+    #   @fb_percentage = (@user.get_facebook_photo_dirt.size / @user.total_fb_photo_count.to_f).round(3)
+    # end
     render :results
   end
 
