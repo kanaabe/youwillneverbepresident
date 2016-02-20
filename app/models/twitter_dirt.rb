ea072d3bfb2b7c4d56cd0730b3dfaa2ed76fc190
joshuabamboo <joshuaowens011@gmail.com>
2016-02-20 14:31:46 -0500

    remove puts statements from the api debugging session

--- app/models/twitter_dirt.rb
+++ app/models/twitter_dirt.rb
@@ -1,7 +1,6 @@
 class TwitterDirt
   attr_accessor :twitter_client, :handle, :tweets_per_page
   def initialize(handle)
-    p "TwitterDirt.new"
     @twitter_client = initialize_twitter_client
     @tweets_per_page = 200.0
     @handle = handle
@@ -16,51 +15,42 @@ class TwitterDirt
   end
 
   def number_of_pages
-    p "TwitterDirt#number_of_pages"
     page_count = (((twitter_client.user(handle).tweets_count)/tweets_per_page)).ceil
-    binding.pry
+    page_count > 16 ? 16 : page_count
   end
 
   def get_user_timeline
-    p 'TwitterDirt#get_user_timeline'
     tweets = twitter_client.user_timeline(handle, :count => tweets_per_page)
     last_tweet = tweets.last.id
     i=0
     (number_of_pages - 1).times do
-      p 'TwitterDirt#get_user_timeline loop'
       i+=1
       tweet_batch = twitter_client.user_timeline(handle, :count => tweets_per_page, :max_id => last_tweet )
       tweets << tweet_batch
       last_tweet = tweets.flatten.last.id
     end
-    p "TwitterDirt#get_user_timeline loop count: #{i}"
     tweets.flatten.uniq
   end
 
   def obscene_tweets
-    p 'TwitterDirt#obscene_tweets'
     embedded_tweets = []
     i=0
     get_user_timeline.each do |tweet|
-      p 'TwitterDirt#obscene_tweets loop'
       i+=1
       if Obscenity.profane?(tweet.text)
         embedded_tweets << tweet
       end
     end
-    p "TwitterDirt#obscene_tweets loop count: #{i}"
     embedded_tweets
   end
 
 
   def twitter_client
-    p '! Hit Twiter API'
     @twitter_client
   end
 
   private
     def initialize_twitter_client
-      p "!!! TwitterDirt#initialize_twitter_client"
       client = Twitter::REST::Client.new do |config|
         config.consumer_key        = ENV['CONSUMER_KEY']
         config.consumer_secret     = ENV['CONSUMER_SECRET']
