6f72ea760257c23aa389345af340d05f8c50645d
joshuabamboo <joshuaowens011@gmail.com>
2016-02-20 22:50:21 -0500

    remove debugging puts statements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,10 @@ class UsersController < ApplicationController
     @user = User.new
     if session[:twitter_handle] != ""
       dirt = TwitterDirt.new(session[:twitter_handle])
-      @tweets = dirt.obscene_tweets; puts "Users#index line 15"
-      @all_tweets = dirt.timeline_size; puts "Users#index line 16"
-      @all_tweets.size > 0 ? @twitter_percentage = (@tweets.size.to_f/@all_tweets.size.to_f).round(3) : @twitter_percentage = 0; puts "Users#index line 17"
-      @twitter_photo = dirt.get_twitter_photo; puts "Users#index line 18"
+      @tweets = dirt.obscene_tweets
+      @all_tweets = dirt.timeline_size
+      @all_tweets.size > 0 ? @twitter_percentage = (@tweets.size.to_f/@all_tweets.size.to_f).round(3) : @twitter_percentage = 0
+      @twitter_photo = dirt.get_twitter_photo
     end
 
     render :results
